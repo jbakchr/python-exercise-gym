5ad85ab781066b0b6a4305f57d619be2dae937a3
Jonas Bak Phillipson <jbakchr@gmail.com>
2026-08-10 13:53:57 +0200

    refactor(03-return-a-function): streamline docstring

--- decorators/1-foundations/solutions/03-return-a-function.py
+++ decorators/1-foundations/solutions/03-return-a-function.py
@@ -1,75 +1,26 @@
 """
 Exercise 03 - Return a Function
 
-The goal of this exercise is to understand that functions
-can return other functions.
+Functions can be created inside other functions and
+returned as values.
 
-This is another important building block behind decorators.
-
-A decorator eventually returns a new function (often called
-a wrapper), so understanding this concept first makes
-decorators much easier to understand later.
+This is one of the fundamental building blocks behind
+decorators.
 """
 
 
 def create_greeter():
-    """
-    Create and return a greeting function.
-    """
+    """Create and return a greeting function."""
 
     def greet():
         """Print a greeting."""
         print("Hello!")
 
-    # Return the function object itself.
-    #
-    # Notice:
-    # We use:
-    #     return greet
-    #
-    # NOT:
-    #     return greet()
-    #
-    # Returning greet() would execute the function immediately
-    # and return its result instead of returning the function.
     return greet
 
 
-# Call create_greeter().
-#
-# This returns the nested greet() function.
+# Store the returned function in a variable.
 my_greeter = create_greeter()
-
 
 # Execute the returned function.
 my_greeter()
-
-
-# ------------------------------------------------------------------
-# Additional experiments
-# ------------------------------------------------------------------
-
-print("\nCalling the returned function again:")
-my_greeter()
-
-
-def create_farewell():
-    """
-    Create and return a farewell function.
-    """
-
-    def goodbye():
-        print("Goodbye!")
-
-    return goodbye
-
-
-farewell = create_farewell()
-
-print("\nCalling a different returned function:")
-farewell()
-
-
-print("\nInspecting returned function objects:")
-print(my_greeter)
-print(farewell)
