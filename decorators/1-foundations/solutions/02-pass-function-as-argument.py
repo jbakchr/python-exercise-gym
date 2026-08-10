8fd996f046498bc6db470644ff1958488e1d53a1
Jonas Bak Phillipson <jbakchr@gmail.com>
2026-08-10 12:50:55 +0200

    refactor(decorators): simplify pass-function-as-argument example

--- decorators/1-foundations/solutions/02-pass-function-as-argument.py
+++ decorators/1-foundations/solutions/02-pass-function-as-argument.py
@@ -1,8 +1,8 @@
 """
 Exercise 02 - Pass Function as Argument
 
-The goal of this exercise is to understand that functions
-can be passed to other functions just like any other value.
+Functions can be passed to other functions just like
+any other value.
 
 This concept is one of the fundamental building blocks
 behind decorators.
@@ -15,61 +15,11 @@ def say_hello():
 
 
 def run(action):
-    """
-    Execute the function that was passed in.
-
-    Parameters
-    ----------
-    action : function
-        The function to execute.
-    """
+    """Execute the function that was received."""
     action()
 
 
-# Pass the function object into run().
-#
-# Notice:
-# We pass say_hello itself, not say_hello().
-#
-# Correct:
-#     run(say_hello)
-#
-# Incorrect:
-#     run(say_hello())
-#
-# The incorrect version executes the function immediately
-# before run() receives it.
+# Pass the function into run().
+# Notice that we pass the function itself,
+# not the result of calling the function.
 run(say_hello)
-
-
-# ------------------------------------------------------------------
-# Additional experiments
-# ------------------------------------------------------------------
-
-
-def say_goodbye():
-    """Print a farewell message."""
-    print("Goodbye!")
-
-
-print("\nRunning another function:")
-run(say_goodbye)
-
-
-def say_name():
-    """Print a name."""
-    print("Jonas")
-
-
-print("\nRunning a third function:")
-run(say_name)
-
-
-def run_twice(action):
-    """Execute the supplied function two times."""
-    action()
-    action()
-
-
-print("\nRunning the same function twice:")
-run_twice(say_hello)
