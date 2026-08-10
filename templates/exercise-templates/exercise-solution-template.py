706951ec989c2c370e4f4124754fec5f8cc58424
Jonas Bak Phillipson <jbakchr@gmail.com>
2026-08-10 12:54:18 +0200

    feat(templates): add exercise solution template file

---
+++ templates/exercise-templates/exercise-solution-template.py
@@ -0,0 +1,29 @@
+"""
+Exercise XX - <Exercise Title>
+
+Briefly explain the core concept being practiced.
+
+Keep this description short.
+
+Example:
+
+Functions can be passed to other functions just like
+any other value.
+
+This concept is one of the fundamental building blocks
+behind decorators.
+"""
+
+
+def main():
+    """
+    Implement the exercise solution here.
+
+    Use clear code.
+    Avoid unnecessary complexity.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
